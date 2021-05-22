a62d81ee63278f5dc4f9e7a33381b1ceed91f7ec
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-05-22 10:32:29 -0300

    chore: add prettier support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 // import styles from '../styles/Home.module.css'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const bingo = "bingo"
+  const bingo = 'chinba';
+  const bing = 'chinba';
   return (
     <div className={styles.container}>
       <Head>
@@ -19,8 +20,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by editing <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
@@ -34,10 +34,7 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -47,9 +44,7 @@ export default function Home() {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
@@ -67,5 +62,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
